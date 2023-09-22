b4e5fbd2c3300f12c1c6f43254e9ba5b09642142
Dakota Stapleton <145411948+dbstapleton@users.noreply.github.com>
2023-09-22 17:09:43 -0500

    Add files via upload

---
+++ active.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const navigationLinks = document.querySelectorAll("header nav a");
+
+    window.addEventListener("scroll", function () {
+        let currentSectionId = "";
+        for (const section of document.querySelectorAll("main section")) {
+            const sectionTop = section.offsetTop - 50;
+            const sectionBottom = sectionTop + section.clientHeight;
+
+            if (window.scrollY >= sectionTop && window.scrollY < sectionBottom) {
+                currentSectionId = section.id;
+                break;
+            }
+        }
+
+        navigationLinks.forEach((link) => {
+            link.classList.remove("active");
+        });
+
+        const activeLink = document.querySelector(`header nav a[href="#${currentSectionId}"]`);
+        if (activeLink) {
+            activeLink.classList.add("active");
+        }
+    });
+});
